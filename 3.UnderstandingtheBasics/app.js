c778f9d151e10a40a5d3b65d372bb9b4c0f1b9ba
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-10-04 12:20:07 +0100

    3.simple headers

--- 3.UnderstandingtheBasics/app.js
+++ 3.UnderstandingtheBasics/app.js
@@ -1,29 +1,40 @@
 const http = require("http");
+const fs = require("fs");
 
 //Criaçao de um servidor:
 const server = http.createServer((req, res) => {
   //console.log(req.url, req.method, req.headers);
 
   const url = req.url;
+  const method = req.method;
   if (url == "/") {
     //quit the process:
     //process.exit();
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>My first Page Node JS</title></head>");
-    res.write("<body><h1>TEXT</h1></body>");
+
+    res.write(
+      "<body><form method='POST' action='/message' ><input type='text' name='go to message'></input></form></body>"
+    );
     res.write("</html>");
     return res.end();
-  } else {
-    //quit the process:
-    //process.exit();
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My first Page Node JS</title></head>");
-    res.write("<body><h1>Another page</h1></body>");
-    res.write("</html>");
-    res.end();
   }
+
+  if (url === "/message" && method == "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  //quit the process:
+  //process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first Page Node JS</title></head>");
+  res.write("<body><h1>Another page</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 //Listening in port localhost : 3000
