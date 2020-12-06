e5c9ef6579489ea278edd654d6e0c164a6d1359a
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-12-06 13:50:34 -0300

    routes

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const http = require("http");
+//const http = require("http");
 
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use("/special", (req, res, next) => {
   console.log("Im middleware!");
 
-  next();
+  res.send("<h1>HELLO Special</h1>");
 });
 
-app.use((req, res, next) => {
-  console.log("Im middleware! 2");
+app.use("/", (req, res, next) => {
+  console.log("Im middleware!");
+
+  res.send("<h1>HELLO</h1>");
 });
 
 const routes = require("./router");
 
 //Criaçao de um servidor:
-const server = http.createServer(app);
+//const server = http.createServer(app);
 //console.log(req.url, req.method, req.headers)
 //Listening in port localhost : 3000
-server.listen(3000);
+//server.listen(3000);
+
+app.listen(3000);
