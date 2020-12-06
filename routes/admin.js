f484b2dcff426202681cff620eaf5808edbfdd24
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-12-06 19:55:27 +0000

    Pug template

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,8 @@ const rootDir = require("../resources/path");
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { docTitle: "Add Product" });
+  //res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
