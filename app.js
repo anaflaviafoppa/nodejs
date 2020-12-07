c3d22755b0162fef17c533da0d2c4e4960322527
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-12-07 20:56:02 +0000

    CAP -8

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
-//const http = require("http");
+const path = require('path');
 
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-//sharing data around the app:
-//view engine
-app.set("view engine", "pug");
-//onde está as views:
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//routers
-
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-
-//Controller:
-const get404Router = require("./controllers/404");
-
-//static router - css files in public folder for example
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/admin", adminRoutes);
-
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-//404 page:
-app.use(get404Router.get404);
-
-//Criaçao de um servidor:
-//const server = http.createServer(app);
-//console.log(req.url, req.method, req.headers)
-//Listening in port localhost : 3000
-//server.listen(3000);
+app.use(errorController.get404);
 
 app.listen(3000);
