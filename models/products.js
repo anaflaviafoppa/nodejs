d6f3e02a8682ee21a70af7f089222b39856c7664
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-12-07 19:57:39 +0000

    helper funcion

--- models/products.js
+++ models/products.js
@@ -1,25 +1,31 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const helperFunction = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-
+    return helperFunction((products) => {
       products.push(this);
+
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
@@ -27,18 +33,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-
-      cb(JSON.parse(fileContent));
-    });
+    return helperFunction(cb);
   }
 };
